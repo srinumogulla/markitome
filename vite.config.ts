71afecb04653aa0bab049985c1cd5eb6abe3592c
srinimogu <mogulla.srinivas@gmail.com>
2026-02-26 13:05:13 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/markitome/",   // 👈 ADD THIS LINE
+  base: "/markitome/",
 
   server: {
     host: true,
