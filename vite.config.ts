8349a775484307e0eec83445ad84fb91ee497715
srinimogu <mogulla.srinivas@gmail.com>
2026-02-26 13:36:40 +0530

    Switch to HashRouter for GitHub Pages routing fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/markitome/",
+  base: "./",   // 👈 CHANGE THIS
 
   server: {
     host: true,
