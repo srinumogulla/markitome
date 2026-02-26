f8ad344a70366b190e5dc74cdcded05d916ee5df
srinimogu <mogulla.srinivas@gmail.com>
2026-02-26 12:57:17 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/markitome/",   // 👈 ADD THIS LINE
+
   server: {
     host: true,
     port: 8080,
   },
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
